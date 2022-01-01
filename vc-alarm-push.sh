2789f141b4b456e3fb4d42e7ff7e6523163c148f
vwired <31988998+virtuallywired@users.noreply.github.com>
2022-01-01 23:25:00 +1100

    Initial Commit

---
+++ vc-alarm-push.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Author: Nicholas Mangraviti
+# Website: virtuallywired.io
+# Description: Sends vCenter Alarm Push notifications using Alertzy.app
+# Reference: http://virtuallywired.io
+
+key="<You Alertzy Account Key"
+title="vCenter Alarm"
+folder="Production"
+
+text="Alarm: $1
+Target: $2
+Summary: $5"
+ 
+curl -s \
+    --form-string "accountKey=$key" \
+    --form-string "title=$title" \
+    --form-string "message=$text" \
+    --form-string "group=$folder" \
+https://alertzy.app/send
